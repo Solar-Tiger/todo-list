352b641dae218188b4a55d98850d002e350b8a19
David Singleton <davidjsingleton18@gmail.com>
2024-06-13 10:26:39 -0700

    Add delete todo projects function and attach it to global namespace

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import './style.css';
 import { addTodoToArray as createProject } from './modules/todo_projects/addProjectToArray.js';
 import { addTodoTaskToArray as createTask } from './modules/todo_task/addTaskToProject.js';
+import {deleteTodoProject} from './modules/todo_projects/deleteTodoProject.js'
 
-const todoProjects = [];
+let todoProjects = [];
 
 // EXAMPLE
 createProject(todoProjects, 'Food to make');
 
 createProject(todoProjects, 'Games to play');
 
-window.createProject = createProject;
 window.todoProjects = todoProjects;
+window.createProject = createProject;
 window.createTask = createTask;
+window.deleteTodoProject = deleteTodoProject
 
 createTask(
   todoProjects[1].id,
@@ -21,3 +23,5 @@ createTask(
   new Date(),
   'VERY HIGH'
 );
+
+deleteTodoProject(todoProjects[0].id, todoProjects);
