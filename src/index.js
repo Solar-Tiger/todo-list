8cfc6c3b9b8440ce8c696d9e1d0ed2ffc06b74d9
David Singleton <davidjsingleton18@gmail.com>
2024-06-14 10:35:23 -0700

    Update the way we get the todo control logic and separate it from the index.js file for better organization via grouping

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
 import './style.css';
-import { addTodoToArray as createProject } from './modules/todo_projects/addProjectToArray.js';
 import { addTodoTaskToArray as createTask } from './modules/todo_task/addTaskToProject.js';
-import { deleteTodoProject } from './modules/todo_projects/deleteTodoProject.js';
-import { todoProjects } from './modules/todo_projects/addProjectToArray.js';
+import { deleteTodoTask } from './modules/todo_task/deleteTodoTask.js';
+import {
+  getTodoProjects,
+  addTodoToArray as createTodoProject,
+  deleteTodoProject,
+} from './modules/todoProjectController.js';
 
-createProject(todoProjects, 'Food to make');
+// EXAMPLE
+createTodoProject(getTodoProjects(), 'Food to make');
 
-createProject(todoProjects, 'Games to play');
+createTodoProject(getTodoProjects(), 'Games to play');
 
-window.todoProjects = todoProjects;
-window.createProject = createProject;
-window.createTask = createTask;
+createTodoProject(getTodoProjects(), 'Places to visit');
+
+deleteTodoProject(getTodoProjects()[0].id, getTodoProjects());
+
+window.getTodoProjects = getTodoProjects;
+window.createTodoProject = createTodoProject;
 window.deleteTodoProject = deleteTodoProject;
+window.createTask = createTask;
+window.deleteTodoTask = deleteTodoTask;
 
 createTask(
-  todoProjects[1].id,
-  todoProjects,
+  getTodoProjects()[1].id,
+  getTodoProjects(),
   'Crash Bandicoot 1',
   "A game I've been meaning to play",
-  new Date(),
+  new Date().toDateString(),
   'VERY HIGH'
 );
 
-deleteTodoProject(todoProjects[0].id, todoProjects);
+createTask(
+  getTodoProjects()[1].id,
+  getTodoProjects(),
+  'Crash Bandicoot 2: Cortex Strikes Back',
+  "A sequel to the game I've been meaning to play",
+  new Date().toDateString(),
+  'VERY HIGH'
+);
+
+createTask(
+  getTodoProjects()[1].id,
+  getTodoProjects(),
+  'Rayman Legends',
+  "Another sequel to a game I've been meaning to play",
+  new Date().toDateString(),
+  'VERY HIGH'
+);
+
+deleteTodoTask(
+  getTodoProjects()[1].id,
+  getTodoProjects()[1].task[1].id,
+  getTodoProjects()
+);
