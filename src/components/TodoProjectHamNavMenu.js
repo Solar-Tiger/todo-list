c10d1fe2342007c38a5ec57df5061a2fe9c869de
David Singleton <davidjsingleton18@gmail.com>
2024-06-27 10:58:56 -0700

    Add hamburger menu for mobile display

---
+++ src/components/TodoProjectHamNavMenu.js
@@ -0,0 +1,24 @@
+export function loadNavHamburgerMenu() {
+  const todoContent = document.querySelector('#todo-content');
+
+  // Create and append nav bar to todo content container
+  const nav = document.createElement('nav');
+
+  nav.classList.add('todo-projects-sidebar-two');
+
+  todoContent.appendChild(nav);
+
+  // Create and append ham menu onctainer to nav bar
+  const div = document.createElement('div');
+
+  div.classList.add('ham-menu');
+
+  nav.appendChild(div);
+
+  // Create and append 3 span elements to ham menu container
+  for (let i = 0; i < 3; i++) {
+    const span = document.createElement('span');
+
+    div.appendChild(span);
+  }
+}
