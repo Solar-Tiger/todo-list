6d70ce2f1e05cd237ccf59902396c66b593d51e5
David Singleton <davidjsingleton18@gmail.com>
2024-07-19 10:53:47 -0700

    Add modal displayer for adding todo projects via dom

--- src/components/TodoProjectModal.js
+++ src/components/TodoProjectModal.js
@@ -1,5 +1,64 @@
+import { addTodoProjectToSidebar } from '../modules/todo_project_adders/todoProjectAdd';
+
 function todoProjectModal() {
+  // Create and append dialog to TODO content
+  const todoContent = document.querySelector('#todo-content');
+
   const dialog = document.createElement('dialog');
 
-  dialog.dataset = 'TEST';
+  dialog.id = 'todo-project-dialog';
+
+  todoContent.appendChild(dialog);
+
+  // Create and append form to dialog
+  const form = document.createElement('form');
+
+  form.action = '';
+  form.method = 'get';
+  form.class = 'todo-project-form';
+
+  dialog.appendChild(form);
+
+  // Create and append form contents to form
+  const projectName = document.createElement('label');
+
+  projectName.for = 'project-name';
+  projectName.textContent = 'Enter Project Name:';
+
+  const projectNameInput = document.createElement('input');
+
+  projectNameInput.type = 'text';
+  projectNameInput.id = 'project-name';
+  projectNameInput.name = 'project-name';
+
+  form.append(projectName, projectNameInput);
+
+  // Create and append confirm or deny button container to dialog
+
+  const btnContainer = document.createElement('div');
+
+  const confirmBtn = document.createElement('button');
+  const cancelBtn = document.createElement('button');
+
+  confirmBtn.textContent = 'Add Project';
+  confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addTodoProjectToSidebar(dialog, projectNameInput);
+  });
+
+  cancelBtn.textContent = 'Cancel';
+  cancelBtn.formMethod = 'dialog';
+
+  btnContainer.append(confirmBtn, cancelBtn);
+
+  form.appendChild(btnContainer);
 }
+
+function showDialogModal() {
+  const todoProjectAddDialog = document.querySelector('#todo-project-dialog');
+
+  todoProjectAddDialog.showModal();
+}
+
+export { todoProjectModal, showDialogModal };
