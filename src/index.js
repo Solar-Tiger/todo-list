f855de82076bbc434dfa5111f9f7bd7e1b09a3f6
David Singleton <davidjsingleton18@gmail.com>
2024-07-22 10:31:16 -0700

    Add filler task to each default todo project

--- src/index.js
+++ src/index.js
@@ -37,11 +37,89 @@ window.deleteTodoProject = deleteTodoProject;
 window.createTodoTask = createTodoTask;
 window.deleteTodoTask = deleteTodoTask;
 
+// Food added to the "Food to make" TODO Project
+
+createTodoTask(
+  getTodoProjects()[0].id,
+  getTodoProjects(),
+  'Chicken pot pie',
+  'A southern favorite dinner-like pie',
+  new Date().toDateString(),
+  'LOW'
+);
+
+createTodoTask(
+  getTodoProjects()[0].id,
+  getTodoProjects(),
+  'Stromboli',
+  'A burrito like folded pizza',
+  new Date().toDateString(),
+  'MEDIUM'
+);
+
+// Games added to the "Games to play" TODO Project
+
 createTodoTask(
   getTodoProjects()[1].id,
   getTodoProjects(),
   'Crash Bandicoot 1',
-  "A game I've been meaning to play",
+  'The first game in the Crash Bandicoot series for the PS1',
+  new Date().toDateString(),
+  'MEDIUM'
+);
+
+createTodoTask(
+  getTodoProjects()[1].id,
+  getTodoProjects(),
+  'Ratchet & Clank: Rift Apart',
+  'The most recent installment in the Ratcht & Clank series for PS5 and recently released on Steam!',
+  new Date().toDateString(),
+  'VERY HIGH'
+);
+
+createTodoTask(
+  getTodoProjects()[1].id,
+  getTodoProjects(),
+  'Darksiders',
+  'The first game in a series of games featuring the 4 horsemen of the apocalypse',
+  new Date().toDateString(),
+  'HIGH'
+);
+
+// Places to visit add to the "Places to visit" TODO Project
+
+createTodoTask(
+  getTodoProjects()[2].id,
+  getTodoProjects(),
+  'Paris, France',
+  'The city of love and romance but also home to a bunch of underground tunnels decorated with bones!',
+  new Date().toDateString(),
+  'LOW'
+);
+
+createTodoTask(
+  getTodoProjects()[2].id,
+  getTodoProjects(),
+  'Tokyo, Japan',
+  'Home of everything anime and video game',
+  new Date().toDateString(),
+  'HIGH'
+);
+
+createTodoTask(
+  getTodoProjects()[2].id,
+  getTodoProjects(),
+  'Orlando, Florida',
+  'Disney Land! Or is it Disney World? I never remember!',
+  new Date().toDateString(),
+  'MEDIUM'
+);
+
+createTodoTask(
+  getTodoProjects()[2].id,
+  getTodoProjects(),
+  'Austin, Texas',
+  'Someone very important to me lives out this way... Only real reason',
   new Date().toDateString(),
   'VERY HIGH'
 );
