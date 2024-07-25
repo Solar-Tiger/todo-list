e2c8e00e8538ed98dbcf078af2f1e9bb8458dffb
David Singleton <davidjsingleton18@gmail.com>
2024-07-25 10:13:01 -0700

    Add way to update displayed tasks for each todo project so long as the projects array length is greater than 0

--- src/modules/todo_project_list_updaters/todoTaskListUpdater.js
+++ src/modules/todo_project_list_updaters/todoTaskListUpdater.js
@@ -1,34 +1,42 @@
-import { getTodoProjects } from "../todo_project_controllers/todoProjectController";
+import { getTodoProjects } from '../todo_project_controllers/todoProjectController';
 
 // Add and update TODO project list via DOM
 function fetchAndUpdateTodoTaskList(currentTodoTasks, clickedProjectIndex) {
-    currentTodoTasks.textContent = '';
+  currentTodoTasks.textContent = '';
 
-    let clickedProjectTasks
+  let clickedProjectTasks;
 
-    if (clickedProjectIndex === undefined) {
-        clickedProjectTasks = getTodoProjects()[0].task
-    } else {
-        clickedProjectTasks = getTodoProjects()[clickedProjectIndex].task
+  if (clickedProjectIndex === undefined) {
+    clickedProjectTasks = getTodoProjects()[0].task;
+  } else {
+    if (getTodoProjects().length === 0) {
+      return;
     }
-
-    clickedProjectTasks.forEach((task) => {
-        const todoTask = document.createElement('li');
-        const todoTaskName = document.createElement('h2')
-        const todoTaskDescription = document.createElement('p')
-        const todoTaskDueDate = document.createElement('p')
-        const todoTaskPriority = document.createElement('p')
-
-        todoTask.classList.add('todo-task');
-        todoTaskName.textContent = task.taskTitle
-        todoTaskDescription.textContent = task.taskDescription
-        todoTaskDueDate.textContent = task.taskDueDate
-        todoTaskPriority.textContent = task.taskPriority
-        
-        todoTask.append(todoTaskName, todoTaskDescription, todoTaskDueDate, todoTaskPriority)
-    
-        currentTodoTasks.appendChild(todoTask);
-      });
+    clickedProjectTasks = getTodoProjects()[clickedProjectIndex].task;
   }
 
-  export { fetchAndUpdateTodoTaskList }
+  clickedProjectTasks.forEach((task) => {
+    const todoTask = document.createElement('li');
+    const todoTaskName = document.createElement('h2');
+    const todoTaskDescription = document.createElement('p');
+    const todoTaskDueDate = document.createElement('p');
+    const todoTaskPriority = document.createElement('p');
+
+    todoTask.classList.add('todo-task');
+    todoTaskName.textContent = task.taskTitle;
+    todoTaskDescription.textContent = task.taskDescription;
+    todoTaskDueDate.textContent = task.taskDueDate;
+    todoTaskPriority.textContent = task.taskPriority;
+
+    todoTask.append(
+      todoTaskName,
+      todoTaskDescription,
+      todoTaskDueDate,
+      todoTaskPriority
+    );
+
+    currentTodoTasks.appendChild(todoTask);
+  });
+}
+
+export { fetchAndUpdateTodoTaskList };
