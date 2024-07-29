a6525cf565e63831e4d992fa4454921aa9474570
David Singleton <davidjsingleton18@gmail.com>
2024-07-29 10:28:57 -0700

    Update spacing for better readability

--- src/modules/todo_project_list_updaters/todoProjectListUpdate.js
+++ src/modules/todo_project_list_updaters/todoProjectListUpdate.js
@@ -1,24 +1,24 @@
-import { getTodoProjects } from "../todo_project_controllers/todoProjectController";
-import deleteProjectIcon from '../../assets/images/icons/delete.svg'
+import { getTodoProjects } from '../todo_project_controllers/todoProjectController';
+import deleteProjectIcon from '../../assets/images/icons/delete.svg';
 
 // Add and update TODO project list via DOM
 function fetchAndUpdateTodoProjectList(currentTodoProjects) {
-    currentTodoProjects.textContent = '';
-  
-    getTodoProjects().forEach((project) => {
-      const li = document.createElement('li');
-      const p = document.createElement('p');
-      const deleteIcon = document.createElement('img');
-  
-      p.textContent = project.projectTitle;
-  
-      deleteIcon.src = deleteProjectIcon;
-      deleteIcon.width = '32';
-  
-      li.append(p, deleteIcon);
-  
-      currentTodoProjects.appendChild(li);
-    });
-  }
+  currentTodoProjects.textContent = '';
 
-  export { fetchAndUpdateTodoProjectList }
+  getTodoProjects().forEach((project) => {
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    const deleteIcon = document.createElement('img');
+
+    p.textContent = project.projectTitle;
+
+    deleteIcon.src = deleteProjectIcon;
+    deleteIcon.width = '32';
+
+    li.append(p, deleteIcon);
+
+    currentTodoProjects.appendChild(li);
+  });
+}
+
+export { fetchAndUpdateTodoProjectList };
