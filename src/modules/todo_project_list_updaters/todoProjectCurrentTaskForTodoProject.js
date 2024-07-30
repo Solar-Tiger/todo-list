bb16ad1ab60215a9375957796c07951ac3621a27
David Singleton <davidjsingleton18@gmail.com>
2024-07-30 11:01:09 -0700

    Update spacing

--- src/modules/todo_project_list_updaters/todoProjectCurrentTaskForTodoProject.js
+++ src/modules/todo_project_list_updaters/todoProjectCurrentTaskForTodoProject.js
@@ -1,15 +1,17 @@
-import { getTodoTasksDOMList } from "../todo_project_controllers/todoTaskController";
-import { fetchAndUpdateTodoTaskList } from "./todoTaskListUpdater";
-  
-  function displayTodoTasksForCurrentTodoProject(currentTodoProjectList) {
-    const clickedTodoProject = currentTodoProjectList.querySelectorAll("li > p")
-    
-    clickedTodoProject.forEach((project, index) => {
-        project.addEventListener("click", () => {
-            fetchAndUpdateTodoTaskList(getTodoTasksDOMList(), index)
-        })
-    })
-  }
-  
-  export { displayTodoTasksForCurrentTodoProject };
-  
+import { getTodoTasksDOMList } from '../todo_project_controllers/todoTaskController';
+import { fetchAndUpdateTodoTaskList } from './todoTaskListUpdater';
+import { projectUpdater } from '../todo_project_controllers/todoProjectController';
+
+function displayTodoTasksForCurrentTodoProject(currentTodoProjectList) {
+  const clickedTodoProject = currentTodoProjectList.querySelectorAll('li > p');
+
+  clickedTodoProject.forEach((project, index) => {
+    project.addEventListener('click', () => {
+      fetchAndUpdateTodoTaskList(getTodoTasksDOMList(), index);
+
+      projectUpdater.updateCurrentDisplayedProject(index);
+    });
+  });
+}
+
+export { displayTodoTasksForCurrentTodoProject };
