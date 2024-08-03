db0582a8579d7827c292d769d34340e33ed08f54
David Singleton <davidjsingleton18@gmail.com>
2024-08-03 14:13:42 -0700

    Update variable to explicitly state what value it's storing

--- src/modules/todo_project_title_updater/todoProjectTitleUpdater.js
+++ src/modules/todo_project_title_updater/todoProjectTitleUpdater.js
@@ -1,12 +1,16 @@
 import { projectUpdater } from '../todo_project_controllers/todoProjectController';
 
 function updateCurrentProjectTitle() {
-  const currentProjecTitle = document.querySelector(
+  const currentTodoProjectTitle = document.querySelector(
     '.todo-projects-sidebar-two h2'
   );
 
-  currentProjecTitle.textContent =
-    projectUpdater.getDisplayedProject().projectTitle;
+  if (projectUpdater.getDisplayedProject() === undefined) {
+    currentTodoProjectTitle.textContent = 'No projects!';
+  } else {
+    currentTodoProjectTitle.textContent =
+      projectUpdater.getDisplayedProject().projectTitle;
+  }
 }
 
 export { updateCurrentProjectTitle };
