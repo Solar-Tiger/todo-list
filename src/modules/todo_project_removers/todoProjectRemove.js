b8e304e6d28d5abbbf984fa2a4396ce3b4752c34
David Singleton <davidjsingleton18@gmail.com>
2024-08-09 10:13:36 -0700

    Update function name to better explain what it's doing

--- src/modules/todo_project_removers/todoProjectRemove.js
+++ src/modules/todo_project_removers/todoProjectRemove.js
@@ -5,7 +5,7 @@ import {
 import { getTodoTasksDOMList } from '../todo_project_controllers/todoTaskController';
 import { fetchAndUpdateTodoProjectList } from '../todo_project_list_updaters/todoProjectListUpdate';
 import { displayTodoTasksForCurrentTodoProject } from '../todo_project_list_updaters/todoProjectCurrentTaskForTodoProject';
-import { fetchAndUpdateTodoTaskList } from '../todo_project_list_updaters/todoTaskListUpdater';
+import { fetchAndUpdateTodoTasksInList } from '../todo_project_list_updaters/todoTaskListUpdater';
 import { deleteAndUpdateCurrentTodoTasks } from './todoTaskRemove';
 import { projectUpdater } from '../todo_project_controllers/todoProjectController';
 import { saveArrayToLocalStorage } from '../../utils/helpers';
@@ -33,7 +33,7 @@ function deleteAndUpdateCurrentTodoProjects(currentTodoProjectList) {
 
       displayTodoTasksForCurrentTodoProject(currentTodoProjectList);
 
-      fetchAndUpdateTodoTaskList(getTodoTasksDOMList(), 0);
+      fetchAndUpdateTodoTasksInList(getTodoTasksDOMList(), 0);
 
       deleteAndUpdateCurrentTodoTasks(getTodoTasksDOMList());
 
