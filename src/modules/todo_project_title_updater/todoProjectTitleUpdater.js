bb2b2b7c2614e6cb1f6a9745e7d82324049cca94
David Singleton <davidjsingleton18@gmail.com>
2024-08-10 09:30:11 -0700

    Revert parameter to take arguments without selectors

--- src/modules/todo_project_title_updater/todoProjectTitleUpdater.js
+++ src/modules/todo_project_title_updater/todoProjectTitleUpdater.js
@@ -1,28 +1,13 @@
-import { projectUpdater } from '../todo_project_controllers/todoProjectController';
-
-function updateCurrentProjectTitle() {
+function updateCurrentProjectTitle(updatedDisplayTitle) {
   const currentTodoProjectTitle = document.querySelector(
     '.todo-projects-sidebar-two h2'
   );
 
-  if (projectUpdater.getDisplayedProject() === undefined) {
-    currentTodoProjectTitle.textContent = 'No projects!';
+  if (updatedDisplayTitle !== undefined) {
+    currentTodoProjectTitle.textContent = updatedDisplayTitle;
   } else {
-    currentTodoProjectTitle.textContent =
-      projectUpdater.getDisplayedProject().projectTitle;
+    currentTodoProjectTitle.textContent = 'No projects!';
   }
 }
 
-function updateCurrentProjectTitleTwo(currenctDisplayedTitle) {
-  const currentTodoProjectTitle = document.querySelector(
-    '.todo-projects-sidebar-two h2'
-  );
-
-  if (currenctDisplayedTitle === undefined) {
-    currentTodoProjectTitle.textContent = 'No projects!';
-  } else {
-    currentTodoProjectTitle.textContent = currenctDisplayedTitle;
-  }
-}
-
-export { updateCurrentProjectTitle, updateCurrentProjectTitleTwo };
+export { updateCurrentProjectTitle };
