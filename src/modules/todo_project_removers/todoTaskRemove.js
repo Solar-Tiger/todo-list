394f00e84c5efad0e193f82f6d21faaed8894638
David Singleton <davidjsingleton18@gmail.com>
2024-08-16 17:12:53 -0700

    Refactor get all todo projects to be stored in a variable for clarity
    Add a function to delete todo tasks even when under a tasks displayer option

--- src/modules/todo_project_removers/todoTaskRemove.js
+++ src/modules/todo_project_removers/todoTaskRemove.js
@@ -1,10 +1,19 @@
 import {
   getOrSetTodoProjects,
   projectUpdater,
+  deleteTodoProject,
 } from '../todo_project_controllers/todoProjectController';
 import { deleteTodoTask } from '../todo_project_controllers/todoTaskController';
-import { fetchAndUpdateTodoTasksInList } from '../todo_project_list_updaters/todoTaskListUpdater';
-import { findArrayIndex, saveArrayToLocalStorage } from '../../utils/helpers';
+import { getOrSetAllTodoTask } from '../todo_project_controllers/todoTaskController';
+import {
+  fetchAndUpdateTodoTasksInList,
+  fetchAndUpdateAllTodoTaskInList,
+} from '../todo_project_list_updaters/todoTaskListUpdater';
+import {
+  findArrayIndex,
+  saveArrayToLocalStorage,
+  getArrayContainingArrayItem,
+} from '../../utils/helpers';
 
 function deleteAndUpdateCurrentTodoTasks(currentTodoTasksList) {
   const currentTodoTaskListDeleteIcon = currentTodoTasksList.querySelectorAll(
@@ -15,29 +24,64 @@ function deleteAndUpdateCurrentTodoTasks(currentTodoTasksList) {
 
   currentTodoTaskListDeleteIcon.forEach((icon, index) => {
     icon.addEventListener('click', () => {
+      const allTodoProjects = getOrSetTodoProjects().getCurrentTodoProjects();
+
       deleteTodoTask(
         projectUpdater.getDisplayedProject().id,
         projectUpdater.getDisplayedProject().tasks[index].id,
-        getOrSetTodoProjects().getCurrentTodoProjects()
+        allTodoProjects
       );
 
       currentTodoTasksList.removeChild(currentTodoTaskListItem[index]);
 
       const currentProjectIndex = findArrayIndex(
         projectUpdater.getDisplayedProject().id,
-        getOrSetTodoProjects().getCurrentTodoProjects()
+        allTodoProjects
       );
 
       fetchAndUpdateTodoTasksInList(currentTodoTasksList, currentProjectIndex);
 
       deleteAndUpdateCurrentTodoTasks(currentTodoTasksList);
 
-      saveArrayToLocalStorage(
-        'todoProjects',
-        getOrSetTodoProjects().getCurrentTodoProjects()
-      );
+      saveArrayToLocalStorage('todoProjects', allTodoProjects);
     });
   });
 }
 
-export { deleteAndUpdateCurrentTodoTasks };
+function deleteAndUpdateAllTodoTasks(currentTodoTasksList) {
+  const currentTodoTaskListDeleteIcon = currentTodoTasksList.querySelectorAll(
+    'li #todo-task-delete-icon'
+  );
+
+  const currentTodoTaskListItem = currentTodoTasksList.querySelectorAll('li');
+
+  currentTodoTaskListDeleteIcon.forEach((icon, index) => {
+    icon.addEventListener('click', () => {
+      const allTodoProjects = getOrSetTodoProjects().getCurrentTodoProjects();
+      const allTodoTasks = getOrSetAllTodoTask().getAllTodoTask();
+
+      console.log(allTodoTasks[index].id);
+
+      deleteTodoTask(
+        getArrayContainingArrayItem(allTodoProjects, allTodoTasks[index].id),
+        allTodoTasks[index].id,
+        allTodoProjects
+      );
+
+      deleteTodoProject(allTodoTasks[index].id, allTodoTasks);
+
+      currentTodoTasksList.removeChild(currentTodoTaskListItem[index]);
+
+      fetchAndUpdateAllTodoTaskInList(
+        currentTodoTasksList,
+        projectUpdater.getDisplayedProject()
+      );
+
+      deleteAndUpdateAllTodoTasks(currentTodoTasksList);
+
+      saveArrayToLocalStorage('todoProjects', allTodoProjects);
+    });
+  });
+}
+
+export { deleteAndUpdateCurrentTodoTasks, deleteAndUpdateAllTodoTasks };
