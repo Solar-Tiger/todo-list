0207c8f0146171b4180e850353f8e18a783cbe48
David Singleton <davidjsingleton18@gmail.com>
2024-08-22 10:37:27 -0700

    Update create todo project functions to reflect 1 parameter
    Update create todo tasks to accept project title to reflect the find array elelment function update

--- src/modules/todo_project_initial_loadout/todoProjectInitialLoadout.js
+++ src/modules/todo_project_initial_loadout/todoProjectInitialLoadout.js
@@ -16,22 +16,16 @@ import { format } from 'date-fns';
 function loadInitialTodoProjects() {
   if (localStorage.getItem('todoProjects') === null) {
     // Initial TODO Projects
-    createTodoProject(
-      getOrSetTodoProjects().getCurrentTodoProjects(),
-      'Food to make'
-    );
+    createTodoProject('Food to make');
 
-    createTodoProject(
-      getOrSetTodoProjects().getCurrentTodoProjects(),
-      'Games to play'
-    );
+    createTodoProject('Games to play');
 
     // Make sure by default the first project selected is loaded
     projectUpdater.updateCurrentDisplayedProject(0);
 
     // Food added to the "Food to make" TODO Project
     createTodoTask(
-      getOrSetTodoProjects().getCurrentTodoProjects()[0].id,
+      getOrSetTodoProjects().getCurrentTodoProjects()[0].projectTitle,
       getOrSetTodoProjects().getCurrentTodoProjects(),
       'Chicken pot pie',
       'A southern favorite dinner-like pie',
@@ -40,7 +34,7 @@ function loadInitialTodoProjects() {
     );
 
     createTodoTask(
-      getOrSetTodoProjects().getCurrentTodoProjects()[0].id,
+      getOrSetTodoProjects().getCurrentTodoProjects()[0].projectTitle,
       getOrSetTodoProjects().getCurrentTodoProjects(),
       'Stromboli',
       'A burrito like folded pizza',
@@ -50,7 +44,7 @@ function loadInitialTodoProjects() {
 
     // Games added to the "Games to play" TODO Project
     createTodoTask(
-      getOrSetTodoProjects().getCurrentTodoProjects()[1].id,
+      getOrSetTodoProjects().getCurrentTodoProjects()[1].projectTitle,
       getOrSetTodoProjects().getCurrentTodoProjects(),
       'Crash Bandicoot 1',
       'The first game in the Crash Bandicoot series for the PS1',
@@ -59,7 +53,7 @@ function loadInitialTodoProjects() {
     );
 
     createTodoTask(
-      getOrSetTodoProjects().getCurrentTodoProjects()[1].id,
+      getOrSetTodoProjects().getCurrentTodoProjects()[1].projectTitle,
       getOrSetTodoProjects().getCurrentTodoProjects(),
       'Ratchet & Clank: Rift Apart',
       'The most recent installment in the Ratcht & Clank series for PS5 and recently released on Steam!',
@@ -68,7 +62,7 @@ function loadInitialTodoProjects() {
     );
 
     createTodoTask(
-      getOrSetTodoProjects().getCurrentTodoProjects()[1].id,
+      getOrSetTodoProjects().getCurrentTodoProjects()[1].projectTitle,
       getOrSetTodoProjects().getCurrentTodoProjects(),
       'Darksiders',
       'The first game in a series of games featuring the 4 horsemen of the apocalypse',
