11cc632f274552c87e040f7fa3e9ab3523c054f6
David Singleton <davidjsingleton18@gmail.com>
2024-08-23 10:13:10 -0700

    Add secondary title update displayer for current selected todo project on sidebar

--- src/modules/todo_project_title_updater/todoProjectTitleUpdater.js
+++ src/modules/todo_project_title_updater/todoProjectTitleUpdater.js
@@ -2,11 +2,16 @@ function updateCurrentProjectTitle(updatedDisplayTitle) {
   const currentTodoProjectTitle = document.querySelector(
     '.todo-projects-sidebar-two h2'
   );
+  const currentSelectedTodoProject = document.querySelector(
+    '.current-selected-todo-project'
+  );
 
   if (updatedDisplayTitle !== undefined) {
     currentTodoProjectTitle.textContent = updatedDisplayTitle;
+    currentSelectedTodoProject.textContent = updatedDisplayTitle;
   } else {
     currentTodoProjectTitle.textContent = 'No projects!';
+    currentSelectedTodoProject.textContent = 'No projects!';
   }
 }
 
