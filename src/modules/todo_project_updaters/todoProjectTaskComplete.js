5ff3b1f8ce99f62085a225f3764f940bd922d77b
David Singleton <davidjsingleton18@gmail.com>
2024-08-29 10:24:59 -0700

    Create function that marks tasks complete or not when the checkmark is clicked

--- src/modules/todo_project_updaters/todoProjectTaskComplete.js
+++ src/modules/todo_project_updaters/todoProjectTaskComplete.js
@@ -1,8 +1,6 @@
-// Add event delgation to checkmark button
-
-// When checkmark button is clicked strikethrough and darken card
-
-// Retain checkmark when projects are loaded up again
+import { getOrSetTodoProjects } from '../todo_project_controllers/todoProjectController';
+import { getOrSetAllTodoTask } from '../todo_project_controllers/todoTaskController';
+import { saveArrayToLocalStorage } from '../../utils/helpers';
 
 function toggleTaskCompletion() {
   const taskListContainer = document.querySelector('#todo-task-list');
@@ -10,8 +8,21 @@ function toggleTaskCompletion() {
   taskListContainer.addEventListener('click', (e) => {
     e.preventDefault();
 
+    const todoProjects = getOrSetTodoProjects().getCurrentTodoProjects();
+    const allTodoTask = getOrSetAllTodoTask().getAllTodoTask();
+
     if (e.target.id === 'todo-task-complete-icon') {
-      e.target.parentElement.parentElement.classList.toggle('task-complete');
+      const correctTask = allTodoTask.find(
+        (task) => task.id === e.target.closest('.todo-task').dataset.taskId
+      );
+
+      const taskCard = e.target.closest('.todo-task');
+
+      correctTask.isComplete = !correctTask.isComplete;
+
+      taskCard.classList.toggle('task-complete');
+
+      saveArrayToLocalStorage('todoProjects', todoProjects);
     }
   });
 }
